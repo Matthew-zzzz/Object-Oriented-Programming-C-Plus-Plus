b6bb3d41ab9a1b8c26628bb9adb4b96cde4fed0b
Matthew Z <mat2.zalavarria@gmail.com>
2025-09-24 23:16:16 -0700

    Implemented code in vector.cpp

--- Lab2/vector.cpp
+++ Lab2/vector.cpp
@@ -7,8 +7,13 @@ using namespace std;
 *
 * @param v - vector of integers
  */
- void printMemVec(...){
-
+ 
+ void printMemVec(const vector<int>& v){
+cout << "Vector - Each int is worth " << sizeof(int) << " bytes\n";
+for (size_t i=0; i < v.size(); i++)
+{
+  cout << "Value " << v[i] << "\t Address" << &v[i] <<"\n";
+}
  }
 
  /** 
@@ -16,36 +21,47 @@ using namespace std;
  *
  * @param v - address to a vector of integers
   */
-  void incVecBy10(...){
+  void incVecBy10(vector<int>& v){
+    for (size_t i=0; i< v.size(); i++) {
+      v[i] = v[i]+10;
+    }
 
   }
 
   int main() {
-    // create a constant integer called SIZE that is of value 5
+ const int SIZE = 5;   // create a constant integer called SIZE that is of value 5
+ vector<int> vec;   // create a vector of integers called vec that can hold up to 5 elements
 
-    // create a vector of integers called vec that can hold up to 5 elements
-
-
-    //use a for loop to populate vec with the values 100 to 104
+   for(int i=0; i < SIZE; i++)//use a for loop to populate vec with the values 100 to 104
+{
+   vec.push_back(i+100);
+}
 
     printf("Before Increment--------------------\n");
     //call printMemVec(...) on vec
-
+printMemVec(vec);
     //call incBy10(...) on vec
+incVecBy10(vec);
 
-    printf("After Increment----------------\n");
+    printf("\nAfter Increment----------------\n");
     //call printMemVec(...) on vec again to view the changes
-
+printMemVec(vec);
     // remove last element of vec
     // (refer to docs https://cplusplus.com/reference/vector/#google_vignette)
 
-    printf("After Pop----------------------\n");
-
+    vec.pop_back();
+    printf("\nAfter Pop----------------------\n");
+printMemVec(vec);
     //append 101 to 102 at the end of vec 
     // (refer to docs https://cplusplus.com/reference/vector/vector/#google_vignette)
 
-    printf("After Push---------------\n");
-    //call printMemVec(...) on vec again to view the changes
+    vec.push_back(101);
+vec.push_back(102);
 
+
+    printf("\nAfter Push---------------\n");
+   
+    //call printMemVec(...) on vec again to view the changes
+printMemVec(vec);
     return 0;
   }
