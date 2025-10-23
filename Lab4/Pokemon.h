f83ef03541e07f24ee84537c94dec09720d28f3f
Matthew Z <mat2.zalavarria@gmail.com>
2025-10-23 14:44:19 -0700

    Finished Lab4

--- Lab4/Pokemon.h
+++ Lab4/Pokemon.h
@@ -11,8 +11,8 @@ class Pokemon {
         Pokemon();
         Pokemon(string name, int hp, int att, int def, vector<string> type);
         // mutators
-        virtual void speak();       // virtual indicates derive class can implement own ver
-        virtual void printStats();
+         virtual void speak();       // virtual indicates derive class can implement own ver//
+         virtual void printStats();
         // accessors
 
     protected:
